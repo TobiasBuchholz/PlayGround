8273a2981ae275bd6650640d03b231aa9bfb7c91
Tobias Buchholz <tobias.buchholz@pressmatrix.de>
2017-06-01 16:37:01 +0200

    + added TestSubscribeForNotificationsOnWorkerThread()

--- PlayGround.ViewModels/MainViewModel.cs
+++ PlayGround.ViewModels/MainViewModel.cs
@@ -1,10 +1,15 @@
-﻿using System.Reactive;
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Reactive;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
+using System.Threading.Tasks;
 using PlayGround.Contracts.Services.HelloWorld;
 using PlayGround.Contracts.ViewModels;
 using PlayGround.Models;
 using ReactiveUI;
+using Realms;
 using Splat;
 
 namespace PlayGround.ViewModels
@@ -18,16 +23,18 @@ namespace PlayGround.ViewModels
 		private ReactiveCommandBase<Unit, HelloWorldModel> loadHelloWorld;
 
 		public MainViewModel(IHelloWorldService helloWorldService)
-		{
-			this.disposables = new CompositeDisposable();
-			this.helloWorldService = helloWorldService ?? Locator.Current.GetService<IHelloWorldService>();
+        {
+            this.disposables = new CompositeDisposable();
+            this.helloWorldService = helloWorldService ?? Locator.Current.GetService<IHelloWorldService>();
 
-			InitCommands();
-			InitProperties();
-			ExecuteCommands();
-		}
+            InitCommands();
+            InitProperties();
+            ExecuteCommands();
 
-		private void InitCommands()
+            TestSubscribeForNotificationsOnWorkerThread();
+        }
+
+        private void InitCommands()
 		{
 			loadHelloWorld = ReactiveCommand.CreateFromObservable(() => helloWorldService.GetHelloWorld());
 			loadHelloWorld.LogThrownExceptions(disposables);
@@ -45,6 +52,48 @@ namespace PlayGround.ViewModels
 		{
 			loadHelloWorld.ExecuteNow(disposables);
 		}
+
+        private static void TestSubscribeForNotificationsOnWorkerThread()
+        {
+            Debug.WriteLine($"subscribe for notifications on UI thread");
+            Realm
+                .GetInstance()
+                .All<HelloWorldModel>()
+                .SubscribeForNotifications((collection, _, __) => Debug.WriteLine($"SubscribeForNotifications called on UI thread: count = {collection.Count}"));
+
+            Observable
+                .Defer(() =>
+	            {
+	                Debug.WriteLine($"subscribe for notifications on worker thread");
+	                Realm
+	                    .GetInstance()
+	                    .All<HelloWorldModel>()
+	                    .SubscribeForNotifications((collection, _, __) => Debug.WriteLine($"SubscribeForNotifications called on worker thread: count = {collection.Count}"));
+
+	                return Observable.Return(Unit.Default);
+	            })
+                .SubscribeOn(RxApp.TaskpoolScheduler)
+                .SubscribeSafe();
+
+            Observable
+                .Defer(async () =>
+	            {
+	                Debug.WriteLine($"processing stuff for a few seconds");
+	                await Task.Delay(TimeSpan.FromSeconds(7));
+
+	                var realm = Realm.GetInstance();
+	                realm.Write(() =>
+	                {
+	                    realm.Add(new HelloWorldModel { Id = realm.All<HelloWorldModel>().ToList().Count() }, true);
+	                    realm.Refresh();
+	                    Debug.WriteLine($"HelloWorldModel added: count = {realm.All<HelloWorldModel>().ToList().Count()}");
+	                });
+
+	                return Observable.Return(Unit.Default);
+	            })
+                .SubscribeOn(RxApp.TaskpoolScheduler)
+                .SubscribeSafe();
+        }
 
 		#region properties
 		public string HelloWorldText => helloWorldText.Value;
