6a396d6f61309b04d15a29d661aee23e98c4bd68
Tobias Buchholz <tobias.buchholz@pressmatrix.de>
2017-09-06 16:13:36 +0200

    added AmazonIAPService

---
+++ PlayGround.UI.Droid/Services/AmazonIAPService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using com.amazon.device.iap.cpt;
+
+namespace PlayGround.UI.Droid.Services
+{
+    public class AmazonIAPService
+    {
+        public async Task<bool> GetProductInfoAsync(params string[] productIds)
+        {
+            var iapService = AmazonIapV2Impl.Instance;
+            var tcs = new TaskCompletionSource<bool>();
+            var skus = new SkusInput { Skus = productIds.ToList() };
+            var requestId = iapService.GetProductData(skus).RequestId;
+
+            GetProductDataResponseDelegator delegator = null;
+            delegator = new GetProductDataResponseDelegator(response => 
+            {
+                if(response.RequestId == requestId) {
+                    var result = GetResultFromResponse(response);
+                    tcs.SetResult(result);
+                    //iapService.RemoveGetProductDataResponseListener(delegator.responseDelegate);
+                }
+            });
+
+            iapService.AddGetProductDataResponseListener(delegator.responseDelegate);
+            return await tcs.Task;
+        }
+
+        private bool GetResultFromResponse(GetProductDataResponse response)
+        {
+            // do something useful
+            return true;
+        }
+
+        /*
+        public async Task<bool> GetProductInfoAsync(params string[] productIds)
+        {
+            await Task.Delay(TimeSpan.FromMilliseconds(1));
+            var iapService = AmazonIapV2Impl.Instance;
+            var tcs = new TaskCompletionSource<bool>();
+            var skus = new SkusInput { Skus = productIds.ToList() };
+            var requestId = iapService.GetProductData(skus).RequestId;
+        
+            GetProductDataResponseDelegator delegator = null;
+            delegator = new GetProductDataResponseDelegator(async response => 
+            {
+                if(response.RequestId == requestId) {
+                    var result = GetResultFromResponse(response);
+                    tcs.SetResult(result);
+                    await Task.Delay(TimeSpan.FromMilliseconds(1));
+                    iapService.RemoveGetProductDataResponseListener(delegator.responseDelegate);
+                }
+            });
+        
+            iapService.AddGetProductDataResponseListener(delegator.responseDelegate);
+            return await tcs.Task;
+        }
+        */
+    }
+}
--- PlayGround.UI.Droid/Views/MainActivity.cs
+++ PlayGround.UI.Droid/Views/MainActivity.cs
@@ -1,5 +1,8 @@
-﻿using Android.App;
+﻿using System;
+using System.Threading.Tasks;
+using Android.App;
 using Android.OS;
+using PlayGround.UI.Droid.Services;
 
 namespace PlayGround.UI.Droid
 {
@@ -10,7 +13,16 @@ namespace PlayGround.UI.Droid
 		{
 			base.OnCreate(savedInstanceState);
 			SetContentView(Resource.Layout.activity_main);
+            TestBillingStuffAsync();
 		}
-	}
+
+        private async Task TestBillingStuffAsync()
+        {
+            var service = new AmazonIAPService();
+            bool didWork1 = await service.GetProductInfoAsync("com.pressmatrix.kundenmag.showcasemag");
+            bool didWork2 = await service.GetProductInfoAsync("com.pressmatrix.kundenmag.showcasemag");
+            System.Diagnostics.Debug.WriteLine($"did work: {didWork1 && didWork2}");
+        }
+    }
 }
 
